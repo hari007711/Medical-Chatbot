53213309713bff25baa78ffb7ffe7bdfcab1c908
Hari <hari0077kumar@gmail.com>
2025-05-25 16:55:43 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',  # Package name should be lowercase and underscore-separated
+    version='0.0.0',               # Use a valid semantic version
+    author='Bappy ahmed',
+    author_email='entbappy73@gmail.com',
+    packages=find_packages(),             # Maps the root package to 'src'
+    install_requires=[],                  # Add dependencies here as a list
+    
+)
---
+++ template.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+# List of files to create
+list_of_files = [
+    "src/__init__.py",
+    "src/helper/__init__.py",  # assuming helper is a package
+    ".env",
+    "setup.py",
+    "app.py",
+    "research/trials.ipynb"
+]
+
+# Iterate and create files/directories
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    file_dir = file_path.parent
+    filename = file_path.name
+
+    if file_dir != Path("."):
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Creating directory: {file_dir} for the file: {filename}")
+
+    if not file_path.exists() or file_path.stat().st_size == 0:
+        with open(file_path, "w") as f:
+            pass
+        logging.info(f"Creating empty file: {file_path}")
+    else:
+        logging.info(f"{filename} already exists")
